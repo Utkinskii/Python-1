183b7c322ba8831b1bc6a355f9913f5ccbfad67a
Utkinskii <sakunov1707@gmail.com>
2023-10-04 15:40:28 +0300

    Добавьте файлы проекта.

---
+++ Python 1/Python_1.py
@@ -0,0 +1,7 @@
+pet_type = input("Enter the type of pet: ")
+pet_age = input("Enter pet's age: ")
+pet_name = input("Enter your pet's name: ")
+
+output_string = f"This {pet_type} by nickname \"{pet_name}\". Age: {pet_age} of the year."
+
+print(output_string)
